b8ea81676b588c908e7948164015e9740455b67a
tasaboia <tainasaboia@gmail.com>
2024-08-20 11:52:12 -0300

    feat versao com typescript

--- src/app/_components/NavBar.js
+++ src/app/_components/NavBar.js
@@ -1,35 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
-export default function NavBar() {
+export default function NavBar({ children }) {
   return (
-    <nav className="relative py-8 px-4 xl:px-10 bg-primary-500 flex justify-center">
-      <div className="w-28">
-        <img src="/assets/logo.png" alt="" />
-      </div>
-      {/* <div className="flex justify-between items-center">
-        <div className="lg:hidden">
-          <button className="navbar-burger flex items-center p-3 rounded">
-            <svg
-              className="text-white block h-4 w-4"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-            >
-              <title>Mobile menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
+    <div>
+      <nav className=" w-full h-16 px-8 xl:p items-center flex bg-black justify-between z-50">
+        <Link className="w-20" href={"/"}>
+          <img src="/assets/logo.png" alt="" />
+        </Link>
 
-        <div className="hidden lg:block">
-          <a
-            className="inline-block py-3 px-8 text-sm text-white hover:text-blue-50 uppercase font-semibold border-2 border-white"
-            href="#"
-          >
-            Contato
-          </a>
-        </div>
-      </div> */}
-    </nav>
+        <Link
+          href={"form"}
+          class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
+        >
+          Pesquisa
+        </Link>
+      </nav>
+      <div>{children}</div>
+    </div>
   );
 }

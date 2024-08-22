22c61e7c30fb631d14aa4d5835bc93183963f454
tasaboia <tainasaboia@gmail.com>
2024-08-22 19:24:02 -0300

    novo layout de grupo vip

--- src/app/_components/NavBar.js
+++ src/app/_components/NavBar.js
@@ -10,10 +10,10 @@ export default function NavBar({ children }) {
         </Link>
 
         <Link
-          href={"form"}
+          href={"event"}
           class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
         >
-          Pesquisa
+          Evento
         </Link>
       </nav>
       <div>{children}</div>
---
+++ src/app/event/page.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function Event() {
+  return (
+    <div className="pt-10 bg-[#f5f5f5]">
+      <div className="px-8 pt-20 pb-10 bg-[#f5f5f5]">
+        <div className="md:max-w-2xl text-center mx-auto">
+          <span className="inline-block mb-3 text-sm text-primary-500 font-bold uppercase tracking-widest">
+            Estamos muito felizes de ter por aqui.
+          </span>
+          <h1 className="font-heading mb-6 text-5xl lg:text-6xl text-orange-500 font-black tracking-tight">
+            Todo homem tem uma jornada a viver
+          </h1>
+          <p className="mb-8 text-xl font-bold">
+            Nós queremos que você viva de forma sábia, relevante e saudável!
+            Antes de entrar no nosso grupo, pedimos que preencha esse rápido
+          </p>
+          <div className="flex flex-wrap justify-center mb-20 -m-2">
+            <div className="w-full md:w-auto p-2">
+              <Link
+                className="block w-full px-8 py-3.5 text-lg text-center text-white font-bold bg-primary-500 hover:bg-orange-500 focus:ring-4 focus:ring-blue-200 rounded-full"
+                href="#grupo-vip"
+              >
+                Iniciar
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="grupo-vip" className="py-10 bg-[#f5f5f5]">
+        <iframe
+          src="https://sbzpt6ah.forms.app/formulario-sem-titulo"
+          width="100%"
+          height="500"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        />
+      </div>
+
+      <div className="py-10 bg-gray-50 overflow-hidden">
+        <div className="container mx-auto px-4">
+          <div className="py-16 px-8 text-center bg-orange-500 rounded-3xl">
+            <div className="md:max-w-lg mx-auto">
+              <h2 className="font-heading mb-6 text-4xl md:text-5xl text-white font-black tracking-tight">
+                Pronto!
+              </h2>
+              <p className="mb-10 text-xl text-blue-100 font-bold">
+                Abaixo está o link para o nosso grupo VIP para o evento do dia
+                31/08, às 09:00hs:
+              </p>
+              <div className="flex flex-wrap justify-center -m-2 mb-4">
+                <div className="w-auto p-2">
+                  <Link
+                    target="_blank"
+                    className="flex justify-center items-center w-full px-8 py-3.5 text-lg text-center text-white font-bold bg-green-900 hover:bg-green-800 focus:ring-4 focus:ring-green-600 rounded-full"
+                    href="https://chat.whatsapp.com/DOsLRK7QNvF2L3zJqTFDXs"
+                  >
+                    <FaWhatsapp classNameName="mr-2" />
+                    Entrar no Grupo
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

fddbf582467b4f469d48d71aaab62785eaa66c55
dnms.tech <tech.dnms@gmail.com>
2024-12-06 20:05:35 +0000

    website retornado

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/comunidade", request.url));
+  // return NextResponse.redirect(new URL("/comunidade", request.url));
+  return NextResponse.next();
 }
 
 export const config = {
